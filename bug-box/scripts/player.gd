d67231db14c9f06e87bb06127789cea317f03ee4
Jenae Matson <jenaematson101@gmail.com>
2026-09-03 14:17:16 -0800

    Camera is controlled by mouse movement

--- bug-box/scripts/player.gd
+++ bug-box/scripts/player.gd
@@ -26,6 +26,8 @@ func _ready() -> void:
 	if (local):
 		# Activate the camera if local
 		$Camera3D.make_current()
+	
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
 	if !(local):
@@ -41,6 +43,8 @@ func process_physics(delta, input_dir, jump):
 		return
 	if peer_id != multiplayer.get_remote_sender_id():
 		return
+	
+	_update_camera(delta)
 
 	# Add the gravity.
 	if not is_on_floor():
@@ -62,3 +66,40 @@ func process_physics(delta, input_dir, jump):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+
+var _mouse_input : bool = false
+var _mouse_rotation : Vector3
+var _rotation_input : float
+var _tilt_input : float
+var _player_rotation : Vector3
+var _camera_rotation : Vector3
+
+func _unhandled_input(event):
+	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	if _mouse_input :
+		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
+		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
+		print(Vector2(_rotation_input,_tilt_input))
+
+@export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
+@export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
+@export var CAMERA_CONTROLLER : Camera3D
+@export var MOUSE_SENSITIVITY : float = 0.5 
+
+func _update_camera(delta):
+	
+	_mouse_rotation.x += _tilt_input * delta
+	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	_mouse_rotation.y += _rotation_input * delta
+	
+	_player_rotation = Vector3(0.0,_mouse_rotation.y,0.0)
+	_camera_rotation = Vector3(_mouse_rotation.x,0.0,0.0)
+	
+	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
+	CAMERA_CONTROLLER.rotation.z = 0.0
+	
+	global_transform.basis = Basis.from_euler(_player_rotation)
+	
+	_rotation_input = 0.0
+	_tilt_input = 0.0
